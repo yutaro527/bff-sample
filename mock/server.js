54b2616c33014f90b495c6304855b9f917a8a30d
YutaroOgawa527 <yutaro.ogawa.527@gmail.com>
2023-02-03 00:26:29 +0900

    new: モックサーバの作成

---
+++ mock/server.js
@@ -0,0 +1,37 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Middleware(前処理)
+server.use(middlewares)
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        req.method = 'GET'
+    }
+    next()
+})
+
+// ルーティングを使用する場合はここに書く
+server.use(jsonServer.rewriter({}))
+server.use(router)
+
+// 後処理
+router.render = function (req, res) {
+    let data = res.locals.data
+    if (Array.isArray(data)) {
+        const lastPath = req.url.split('/').pop();
+        res.send({[lastPath]: data})
+    } else if (!Object.keys(data).length) {
+        res.status(404).send({
+            message: "Not Found"
+        })
+    } else {
+        res.send(data)
+    }
+}
+
+// モックサーバ起動
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
